21f9a66feb5e38d34d6c13945e2fecff4192331a
kickme26 <guna26.98@gmail.com>
2023-12-09 16:13:34 +0530

    day 13 task

--- DAY-12 TASK/index.js
+++ DAY-12 TASK/index.js
@@ -12,19 +12,19 @@ const DivHindi = document.createElement("div");
 DivHindi.setAttribute("id", "hindi");
 
 
-var img1 =  SetImageElement("https://www.91-cdn.com/metareel-images/content/posters-7-1697183289469-qT2xJtJ7Jza5gC4RSiEXWrztZHo.jpg?tr=h-350,q-40&version=5","Leo")
-        
-var img2 =  SetImageElement("https://www.91-cdn.com/metareel-images/content/posters-1697706691-28AzafDajgmd53F2DgMAK2ncu3K.jpg?tr=h-350,q-40&version=5", "Irugapatru")
+var img1 = SetImageElement("https://www.91-cdn.com/metareel-images/content/posters-7-1697183289469-qT2xJtJ7Jza5gC4RSiEXWrztZHo.jpg?tr=h-350,q-40&version=5", "Leo")
 
-var img3 = SetImageElement("https://www.91-cdn.com/metareel-images/content/posters-7-1695260939784-3xz1lfMrFlNtZ4H8DQKvYog8ilR.jpg?tr=h-350,q-40&version=5","Mark Antony")
+var img2 = SetImageElement("https://www.91-cdn.com/metareel-images/content/posters-1697706691-28AzafDajgmd53F2DgMAK2ncu3K.jpg?tr=h-350,q-40&version=5", "Irugapatru")
+
+var img3 = SetImageElement("https://www.91-cdn.com/metareel-images/content/posters-7-1695260939784-3xz1lfMrFlNtZ4H8DQKvYog8ilR.jpg?tr=h-350,q-40&version=5", "Mark Antony")
 
 var img4 = SetImageElement("https://www.91-cdn.com/metareel-images/content/posters-7-1687313047095-t0oUDRi8izy8Hn5yx6tzy55t1z3.jpg?tr=h-350,q-40&version=5", "Hridayam ")
 
-var img5 = SetImageElement("https://www.91-cdn.com/metareel-images/content/posters-7-1686169459892-zZ4qJFfv6UXqb6otLBW2qzOQu4C.jpg?tr=h-350,q-40&version=5","Dhoomam")
+var img5 = SetImageElement("https://www.91-cdn.com/metareel-images/content/posters-7-1686169459892-zZ4qJFfv6UXqb6otLBW2qzOQu4C.jpg?tr=h-350,q-40&version=5", "Dhoomam")
 
-var img6 = SetImageElement("https://www.91-cdn.com/metareel-images/content/posters-7-1692064008738-jFt1gS4BGHlK8xt76Y81Alp4dbt.jpg?tr=h-350,q-40&version=5","Jawan")
+var img6 = SetImageElement("https://www.91-cdn.com/metareel-images/content/posters-7-1692064008738-jFt1gS4BGHlK8xt76Y81Alp4dbt.jpg?tr=h-350,q-40&version=5", "Jawan")
 
-function SetImageElement(url,alt){
+function SetImageElement(url, alt) {
     const img = document.createElement('img');
     img.setAttribute('src', url);
     img.alt = alt;
@@ -50,5 +50,5 @@ DivMalai.append(img4, img5);
 DivHindi.appendChild(HindiMov);
 DivHindi.append(img6);
 
-document.body.append(h1Element,DivTamil,DivMalai,DivHindi)
+document.body.append(h1Element, DivTamil, DivMalai, DivHindi)
 
